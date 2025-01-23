10142ecf1d9e6e23616cac0ec254d9e447e98d19
THUAN\tranm <thuant1002@gmail.com>
2025-01-23 21:33:07 +0700

    Setting System

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+//dotenv config
+dotenv.config();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.get("/", (req, res) => {
+  message: "server running";
+});
+
+//port
+const port = process.env.PORT || 8080;
+
+//listen port
+app.listen(port, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
